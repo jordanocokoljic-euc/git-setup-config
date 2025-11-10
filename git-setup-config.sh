a4fa50a4b3589846a51a52316dca46e1cfc19b2c
Jordan Ocokoljic <jordan.ocokoljic@eucalyptus.vc>
2025-11-10 13:29:51 +1100

    Update script to work when piped curl->sh

--- git-setup-config.sh
+++ git-setup-config.sh
@@ -2,12 +2,21 @@
 
 set -eu
 
+prompt() {
+  printf '%s ' "$1"
+  if [ -r /dev/tty ]; then
+    IFS= read -r "$2" < /dev/tty
+  else
+    IFS= read -r "$2" || true
+  fi
+}
+
 git_name="$(id -F)"
 git_email="$(id -un)@eucalyptus.vc"
 
-echo "No input will fallback to the value in brackets" 
-read -p "Please enter your name [$git_name]: " set_name
-read -p "Please enter your email [$git_email]: " set_email
+echo "Press enter to use to the value in brackets" 
+prompt "Please enter your name [$git_name]:" set_name 
+prompt "Please enter your email [$git_email]:" set_email
 
 if [ -n "$set_name" ]; then git_name="$set_name"; fi
 if [ -n "$set_email" ]; then git_email="$set_email"; fi
